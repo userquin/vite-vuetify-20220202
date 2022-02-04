48ae5ddf5b79eaa9347d696857c44782c00353b0
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2022-02-04 01:05:26 +0100

    chore: cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import { promises as fs } from 'fs'
 import Vuetify from '@vuetify/vite-plugin'
 import Unocss from 'unocss/vite'
 import presetIcons from '@unocss/preset-icons'
@@ -9,24 +8,9 @@ import presetIcons from '@unocss/preset-icons'
 export default defineConfig({
   plugins: [
     vue(),
-    // {
-    //   name: 'vuetify-fix',
-    //   enforce: 'pre',
-    //   resolveId(id) {
-    //     console.log(id)
-    //     if (id === 'vuetify/dist/json/importMap.json') {
-    //       return 'vuetify-import-map.json'
-    //     }
-    //   },
-    //   async load(id) {
-    //     if (id === 'vuetify-import-map.json') {
-    //       return await fs.readFile('node_modules/vuetify/dist/json/importMap.json', 'utf-8')
-    //     }
-    //   }
-    // },
-      Unocss({
-        presets: [presetIcons()]
-      }),
+    Unocss({
+      presets: [presetIcons()]
+    }),
     Vuetify({ styles: 'expose' })
   ]
 })
